a7856c59b4b62d9eb37f2afb981a62b15e392fd1
rjc2505 <rj25052002@gmail.com>
2021-10-19 23:38:45 +0530

    1 version

--- src/pages/homePage/HomePage.js
+++ src/pages/homePage/HomePage.js
@@ -4,21 +4,21 @@ import SampleCard from "../../components/sampleCard/SampleCard";
 function HomePage() {
   return (
     <>
-      <Container fluid>
-        <Row >
-          <Col md={6}>
-            <SampleCard />
-          </Col>
-          <Col md={6}>
-            <SampleCard />
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <SampleCard />
-          </Col>
-        </Row>
-      </Container>
+       <Container fluid >
+         <Row >
+           <Col md={6}>
+             <SampleCard />
+           </Col>
+           <Col md={6}>
+             <SampleCard />
+           </Col>
+         </Row>
+         <Row>
+           <Col>
+             <SampleCard />
+           </Col>
+         </Row>
+       </Container>
     </>
   );
 }
